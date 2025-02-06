b037eb8319c3fe4171c15b7f4d7f0f36152a8603
hymhtm <hymhtm@gmail.com>
2025-02-06 15:59:10 +0900

    Enhance extract_contents.py with financial API integration and additional extraction methods

--- extract_contents.py
+++ extract_contents.py
@@ -1,7 +1,11 @@
+import os
 import re
 import requests
 
+from dotenv import load_dotenv
+
 def extract_company_name(soup):
+
     """extract company name from a given soup object"""
     return soup.find('title').text.strip()
     
@@ -9,8 +13,9 @@ def extract_postal_code(soup):
     """extract postal code from a given soup object"""
     postal_code_pattern = re.compile(r'\d{3}-\d{4}')
 
-def postal_search(postalcode):
-    """search postal code from a given postal code
+def postal_api_search(postalcode):
+    """
+    search postal code from a given postal code
     How to use:
     prefecture = data['addresses'][0]['ja']['prefecture']
     city = data['addresses'][0]['ja']['address1']
@@ -34,7 +39,6 @@ def postal_search(postalcode):
     #API search
     url = f"https://jp-postal-code-api.ttskch.com/api/v1/{postalcode}.json"
     response = requests.get(url)
-    response.raise_for_status()
     if response.status_code == 200:
         data = response.json()
         return data
@@ -69,7 +73,6 @@ def extract_email(soup):
     email_pattern = re.compile(r'[a-zA-Z-0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}')
     return soup.find(string=email_pattern)
 
-
 def extract_employee_count(soup):
     """extract employee count from a given soup obejct"""
     employee_keywords = ["社員数","従業員数","従業員"]
@@ -78,14 +81,90 @@ def extract_employee_count(soup):
             return text
     return None
 
-
-
-
 def extract_capital(soup):
     """extract capital from a given soup object"""
+    capital_keywords = ["資本金","資本"]
+    capital_pattern = re.compile(r'(\d{1,3}?,\d+)*[百千万億]円|\d+[百千万億]*円')
+    return soup.find(string=capital_pattern)
 
-def extract_financial_status(soup):
-    pass
+def extract_financial_status_link(soup):
+    financial_keywords = ["株主", "投資家","決算短信", "IR資料", "IR情報", "IR", "有価証券報告書", "有価証券報告書(PDF)"]
+    for link in soup.find_all('a', href=True):
+        if any(kw in link.text for kw in financial_keywords):
+            return link['href']
+    return None
+
+def financial_api_search(companyname):
+    """
+    search financial status from a given company name
+    using j-Quants API
+    companyname = data['companyname']
+    
+    Args:
+        companyname(str): company name
+    Returns:
+        json: financial status data
+    """
+    API_URL = "https://api.jquants.com"
+    def get_refresh_token():
+        """
+        get refresh token from j-Quants API
+        refresh token is valid for 7 days
+        using e-mail address and password
+        if you dont have e-mail address and password, you can sign up at https://jpx-jquants.com/
+        
+        Args:
+            None
+        Returns:
+            refresh_token(str): refresh token
+        """
+        load_dotenv()
+        jquants_email = os.getenv("JQUANTS_EMAIL")
+        jquants_password = os.getenv("JQUANTS_PASSWORD")
+        
+
+        url = f"{API_URL}/v1/token/auth_user"
+        response = requests.post(url, json={"mailaddress": jquants_email, "password": jquants_password})
+        if response.status_code == 200:
+            refresh_token = response.json()['refreshToken']
+            return refresh_token
+        else:
+            print(f"API検索エラー: {response.status_code}")
+            return None
+    
+    def get_id_token(refresh_token=None):
+        """
+        get id token from j-Quants API
+        id token is valid for 1 day
+        using refresh token
+        
+
+        Args:
+            
+        Returns:
+            id_token(str): id token
+        """
+        if refresh_token is None:
+            refresh_token = get_refresh_token()
+
+        response = requests.post(f'{API_URL}/v1/token/auth_refresh?refresh_token={refresh_token}')
+        if response.status_code == 200:
+            id_token = response.json()['idToken']
+            headers = {'Authorization': f'Bearer {id_token}'}
+            return headers
+        else:
+            print(f"API検索エラー: {response.status_code}")
+            return None
+    
+    def get_financial_status(companyname):
+        """
+        get financial status from a given company name
+        using j-Quants API
+        """
+        headers = get_id_token()
+        
+
+
 
 def find_contact_form(soup, url):
     """find contact form from a given soup object"""
